f2d890d712924c6c8bde398c7a2ba3f34ce59cfc
nityabarwi <95531972+nityabarwi@users.noreply.github.com>
2021-12-25 14:53:59 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{int a;
+cout<<"enter the number:"<<endl;
+cin>>a;
+if(a%2==0)
+cout<<"number is even"<<endl;
+else
+cout<<"number is odd"<<endl;
+}
